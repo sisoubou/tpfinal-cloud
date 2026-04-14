ed7565af29026b203debed6e105ce43497d47a0a
sisoubou <serena.fadda8@gmail.com>
2026-04-14 13:09:52 +0200

    Initialisation du TP

---
+++ mon-app/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+const buildId = import.meta.env.VITE_BUILD_ID ?? 'local'
+const today = new Date().toLocaleDateString('fr-FR')
+</script>
+<template>
+<header>
+<p>Nom : Fadda Serena</p>
+<p>Date : {{ today }}</p>
+<p>Build : {{ buildId }}</p>
+</header>
+</template>
